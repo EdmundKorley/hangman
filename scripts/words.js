64f022b7028d07fac6e5c7f220f1da1880186efb
Edmund Korley <korley.em@gmail.com>
2017-02-16 12:05:27 -0500

    Log localStorage attempt

--- scripts/words.js
+++ scripts/words.js
@@ -1,31 +1,55 @@
+// A global object representing the words
+
 class Words {
   constructor(input) {
+    this.words;
     if (typeof input === "string") {
       this.API = input;
-      this.pullWords = this.grabFromAPI || this.grabFromIframe;
+      this.get = this.grabFromAPI || this.grabFromIframe;
     } else {
-      this.pullWords = this.grabLocally;
+      this.get = this.grabLocally;
     }
-    this.wordsElement = document.getElementById("words");
   }
-  // A hack to bypass CORS being disabled on the server by using an iframe
-  makeIframe(config) {
-    this.wordsElement.innerHTML = `<iframe src="http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words${makeQueryString(config)}" id="renderedWords"></iframe>`;
-  }
-  grabFromIframe(config) {
-    this.makeIframe(config);
-    let iframeDiv = document.getElementById("renderedWords");
-    console.log(iframeDiv);
+  // Grab locally a
+  grabLocally(difficulty = 10) {
+    difficulty = String(difficulty);
+    const store = localStorage.getItem(`hangman-store-${difficulty}`);
+    // Check if we have our JSON object in local storage,
+    // if pick a random word (non-destructively so as
+    // to preserve randomness) to present to user.
+    return new Promise(function(resolve, reject) {
+      if (store !== null) {
+        console.log('CACHE HIT LETS ROLL.');
+        const data = JSON.parse(store);
+        const wordsList = data[difficulty];
+        const word = wordsList[Math.floor(Math.random(0, wordsList.length))];
+        debugger;
+        resolve(word);
+      } else {
+        console.log('I NEED TO FETCH JSON FROM SERVER, UGH HOLD ON.');
+        fetch('words.json')
+          .then(response => {
+            if (response.ok) return response.json();
+            throw new Error('Fetch request failed 😨.');
+          })
+          .then(data => {
+            for (let key in data) {
+              if (data.hasOwnProperty(key)) {
+                localStorage.setItem(`hangman-store-${key}`, JSON.stringify(data[key]));
+              }
+            }
+            const wordsList = data[difficulty];
+            const word = wordsList[Math.floor(Math.random(0, wordsList.length))];
+            resolve(word);
+          })
+          .catch(err => reject(err));
+      }
+    });
   }
 }
 
-// Helper for generating complaint query params
-function makeQueryString(configObject) {
-  let params = []
-  for (let key in configObject) {
-    if (configObject.hasOwnProperty(key)) {
-      params.push(key + "=" + configObject[key]);
-    }
-  }
-  return "?" + params.join("&");
-}
+// A small test of our Words service as is.
+const n = new Words({});
+n.get(10).then(word => {
+  console.log(`Here is my word: ${word}`);
+});

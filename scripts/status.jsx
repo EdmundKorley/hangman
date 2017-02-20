3aefe0e9ba13f78dbc42ebc28015d3fc47348797
Edmund Korley <korley.em@gmail.com>
2017-02-20 00:02:05 -0500

    Components are hooked up nicely for a draft
    Need to fix issues when losing or wining, svg listeners break.

--- scripts/status.jsx
+++ scripts/status.jsx
@@ -4,6 +4,6 @@ Display real-time infomation about the current game session
 
 class Status extends React.Component {
   render() {
-    return (<div className="hangman-status">Guesses left: {6 - this.props.keys.size}</div>);
+    return (<div className="hangman-status">Guesses left: {this.props.guessesLeft}</div>);
   }
 }
